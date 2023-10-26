fa96c6ba507fd9531ec6b0ced09a17410c9eb598
GGallangE <ehrbs4789@gmail.com>
2023-10-26 09:41:52 +0900

    feat:로그인 구현위해 security 설정

---
+++ src/main/java/com/bnksys/esg/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bnksys.esg.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+
+//        http.authorizeHttpRequests().requestMatchers("/spring/**").authenticated();
+        http.authorizeHttpRequests().anyRequest().permitAll();
+
+        return http.build();
+    }
+}
