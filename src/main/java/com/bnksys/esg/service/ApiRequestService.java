b1172f1fcf00c3943ce1ae8eddfa5507311dcb2c
miiin-se <alstp7845@gmail.com>
2024-01-09 10:11:26 +0900

    fix

--- src/main/java/com/bnksys/esg/service/ApiRequestService.java
+++ src/main/java/com/bnksys/esg/service/ApiRequestService.java
@@ -196,7 +196,7 @@ public class ApiRequestService {
             return "ok";
         } else if ("excel".equals(type)) {
             String sanitizedFileName = apinm.replace('/', '_');
-            filePath = Paths.get(downloadFolderPath, sanitizedFileName + ".xml").toString();
+            filePath = Paths.get(downloadFolderPath, sanitizedFileName + ".xlsx").toString();
             generateExcel_Post(combinedResponse, filePath);
             return "ok";
         } else if ("txt".equals(type)) {
